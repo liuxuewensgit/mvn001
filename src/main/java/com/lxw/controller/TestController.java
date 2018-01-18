3fcf17105563fd5aa0b7d5dc9a043214222a8c97
liuxuewensgit <601087524@qq.com>
2018-01-18 18:42:01 +0800

    Update TestController.java

--- src/main/java/com/lxw/controller/TestController.java
+++ src/main/java/com/lxw/controller/TestController.java
@@ -4,5 +4,6 @@ public class TestController {
     public  static  void main(String[] args){
         System.out.println("qwe");
         System.out.println("from git hub test message");
+        System.out.println("from git hub test message1");
     }
 }
