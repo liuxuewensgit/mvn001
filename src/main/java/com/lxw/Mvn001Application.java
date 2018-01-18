3de0bb7d34dec79932592bccdc854dbd0beafd1c
liuxuewensgit <601087524@qq.com>
2018-01-18 12:01:31 +0800

    Initial commit

---
+++ src/main/java/com/lxw/Mvn001Application.java
@@ -0,0 +1,13 @@
+package com.lxw;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Mvn001Application {
+    public static void main (String[] args){
+        SpringApplication.run(Mvn001Application.class);
+    }
+}
