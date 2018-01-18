54eefa27faf827595c3d3303ff9bfeff3679a62d
liuxuewensgit <601087524@qq.com>
2018-01-18 12:13:25 +0800

    Update TestController.java
    来自GitHub的编辑

--- src/main/java/com/lxw/controller/TestController.java
+++ src/main/java/com/lxw/controller/TestController.java
@@ -3,5 +3,6 @@ package com.lxw.controller;
 public class TestController {
     public  static  void main(String[] args){
         System.out.println("qwe");
+        System.out.println("from git hub test message");
     }
 }
